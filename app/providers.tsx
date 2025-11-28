ec0c18e5a1b86cca7adf327d6bcb5b70e7209564
OmarFaruk00 <kaziomarfaruk00@gmail.com>
2025-11-28 13:32:34 +0600

    docs: add JSDoc comment to Providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,9 @@
 
 import { SessionProvider } from 'next-auth/react'
 
+/**
+ * Providers component wraps the app with NextAuth session provider
+ */
 export function Providers({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>
 }
